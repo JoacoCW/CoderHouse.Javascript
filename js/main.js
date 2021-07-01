765cc8049ac9b2150ff2019c45f6bac276613cee
JoaquinWeiss <joaco.w93@gmail.com>
2021-07-01 13:10:33 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function funcion(){
+    let numeroIngresado = prompt('Ingresar un numero. Si no está entre 10 y 50, hay tabla.');
+    // Verifico  -> está entre 10 y 50?
+    let numero = parseInt(numeroIngresado);
+    if((numero >=10 ) && (numero <=50 )){
+        alert('Tu numero es '+numero);
+    }else {
+    //alert('Hay tabla');
+    document.getElementById("img1").src= "assets/img/hay_tabla.jpg";
+    }
+    console.log(parseInt(numero));
+}
