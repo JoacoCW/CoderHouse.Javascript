17d6b06de1dfd941c12b4993e7168c29172389ab
JoaquinWeiss <joaco.w93@gmail.com>
2021-07-16 12:00:36 -0300

    desafío complementario clase 6

---
+++ js/clase06.js
@@ -0,0 +1,49 @@
+// Objeto invidato
+
+class Invitado{
+    constructor (nombre, apellido, documento, dire, menu, confirma) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.documento = 0;
+        this.dire = '';
+        this.menu = '';
+        this.confirma = '';
+    }
+    comer(){
+        console.log(this.nombre + ' ' + this.apellido + ' comerá el menú ' + this.menu);
+    }
+    mostrarInvitado(){
+        document.getElementById("resultados").innerHTML+=`<h4>Se agregó a ${this.nombre} ${this.apellido} a lista de invitados</h4>`;
+    }
+}
+
+// Array de invitados. Más adelante quiero que sea un JSON al que se pueda acceder desde los diferentes scripts
+
+const invitados = [
+    {nombre : "Franco", apellido : "Silva", documento : "39999999", dire : "Calle tal 546", menu : "Clasico", confirma : "si"},
+    {nombre : "Mariana", apellido : "Torreblanca", documento : "38999999", dire : "Calle tal 546", menu : "Clasico", confirma : "si"},
+    {nombre : "Fabricio", apellido : "Silva", documento : "40999999", dire : "", menu : "", confirma : ""},
+    {nombre : "Roxana", apellido : "Podesta", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Sergio", apellido : "Torreblanca", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Cecilia", apellido : "Apellido", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Facundo", apellido : "Torreblanca", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Omar", apellido : "Torreblanca", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Agostina", apellido : "Torreblanca", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Mama de facu", apellido : "Hinojosa", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Katia", apellido : "Itin", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Giovanni", apellido : "Casali", documento : "", dire : "", menu : "", confirma : ""},
+    {nombre : "Joaquín", apellido : "Weiss", documento : "37608601", dire : "Juramento 3675", menu : "Clasico", confirma : "si"}
+];
+
+
+function compare( a, b ) {
+    if ( a.nombre < b.nombre ){
+      return -1;
+    }
+    if ( a.nombre > b.nombre ){
+      return 1;
+    }
+    return 0;
+  }
+  
+  console.log(invitados.sort( compare ));
